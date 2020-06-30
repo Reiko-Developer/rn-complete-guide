8724af589c26f38bc9482045828e92668c9f232d
Reiko-Developer <lukassfifa@gmail.com>
2020-06-30 07:06:37 -0500

    Organized the codes

---
+++ materials/mod 02/final files/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const addGoalHandler = goalTitle => {
+    //This works but not in all the situations.
+    //setCourseGoals([...courseGoals], enteredGoal);
+
+    //this is the best practice, and workds in all situations!
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle },
+    ]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId);
+    });
+  }
+
+  const cancelGoalAddHandler = () => {
+    setIsAddMode(false);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Button title="Add Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAddHandler} />
+      {/*by default the key extractor is looking for item.key, but u can change for id*/}
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value}></GoalItem>
+        )}
+      />
+    </View>
+  );
+}
+
+//The best practice when working with styles is tu use the StyleSheet object!
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+});
